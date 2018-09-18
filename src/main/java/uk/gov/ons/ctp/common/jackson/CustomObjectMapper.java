1d93ca14eb9c4203680259246ce59f5c582799c3
Nick Grant <nick.grant@ext.ons.gov.uk>
2018-09-18 15:52:46 +0100

    Restore formatter for PARSING as well as outputting dates

--- src/main/java/uk/gov/ons/ctp/common/jackson/CustomObjectMapper.java
+++ src/main/java/uk/gov/ons/ctp/common/jackson/CustomObjectMapper.java
@@ -3,14 +3,14 @@ package uk.gov.ons.ctp.common.jackson;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import java.text.SimpleDateFormat;
+import uk.gov.ons.ctp.common.util.MultiIsoDateFormat;
 
 /** Custom Object Mapper */
 public class CustomObjectMapper extends ObjectMapper {
 
   /** Custom Object Mapper Constructor */
   public CustomObjectMapper() {
-    this.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX"));
+    this.setDateFormat(new MultiIsoDateFormat());
     this.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     this.registerModule(new JavaTimeModule());
     this.findAndRegisterModules();
