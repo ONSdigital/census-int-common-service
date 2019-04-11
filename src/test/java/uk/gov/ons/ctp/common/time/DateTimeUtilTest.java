88fa340096a372d2a53b30d50b067f13de8a7451
Peter Bochel <Peter.Bochel@ext.ons.gov.uk>
2019-04-11 09:31:32 +0100

    CR129 Tweaks to test case

--- src/test/java/uk/gov/ons/ctp/common/time/DateTimeUtilTest.java
+++ src/test/java/uk/gov/ons/ctp/common/time/DateTimeUtilTest.java
@@ -6,9 +6,11 @@ import org.junit.Test;
 public class DateTimeUtilTest {
 
   @Test
-  public void testGetCurrentDateTimeInJsonFormat() {
+  public void testGetCurrentDateTimeUsesCorrectFormat() {
+    // Invoke method under test, to get a string with the current datetime
     String currentDateTime = DateTimeUtil.getCurrentDateTimeInJsonFormat();
 
+    // Verify formatting of datatime by parsing it
     String dateTimePattern = DateTimeUtil.DATE_FORMAT_IN_JSON;
     DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(dateTimePattern);
     dateTimeFormatter.parse(currentDateTime);
