b6ba96a3aec323973e5348929ef72e2ffe31565f
Peter Bochel <Peter.Bochel@ext.ons.gov.uk>
2019-06-03 13:34:44 +0100

    CR70 Adding DTO's to hold refusal event

---
+++ src/main/java/uk/gov/ons/ctp/common/event/model/CollectionCaseCompact.java
@@ -0,0 +1,14 @@
+package uk.gov.ons.ctp.common.event.model;
+
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CollectionCaseCompact {
+
+  private UUID id;
+}
