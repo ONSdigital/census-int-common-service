b9a969d12f86b3d0052d6c6d9722eac5429b4324
Phil Whiles <phil.whiles@gmail.com>
2019-10-10 16:40:03 +0100

    CR-569 moved UPRN Editor from CC svc api

---
+++ src/main/java/uk/gov/ons/ctp/common/model/editor/UniquePropertyReferenceNumberEditor.java
@@ -0,0 +1,26 @@
+package uk.gov.ons.ctp.common.model.editor;
+
+import java.beans.PropertyEditorSupport;
+import uk.gov.ons.ctp.common.model.UniquePropertyReferenceNumber;
+
+public class UniquePropertyReferenceNumberEditor extends PropertyEditorSupport {
+
+  @Override
+  public void setAsText(String text) throws IllegalArgumentException {
+    try {
+      Long uprn = Long.parseLong(text);
+      if (uprn.longValue() >= UniquePropertyReferenceNumber.UPRN_MIN && uprn.longValue() <= UniquePropertyReferenceNumber.UPRN_MAX) {
+        setValue(new UniquePropertyReferenceNumber(uprn));
+      } else {
+        throw new IllegalArgumentException();
+      }
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException();
+    }
+  }
+
+  @Override
+  public String getAsText() {
+    return ((UniquePropertyReferenceNumber) this.getValue()).toString();
+  }
+}
