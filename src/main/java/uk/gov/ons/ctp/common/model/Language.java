6eeabc2bb3098ca3db4e3b317104abcaa3a3d5e3
Phil Whiles <phil.whiles@gmail.com>
2019-10-11 16:51:11 +0100

    CR-560 added types for eq launcher

---
+++ src/main/java/uk/gov/ons/ctp/common/model/Language.java
@@ -0,0 +1,21 @@
+package uk.gov.ons.ctp.common.model;
+
+
+public enum Language {
+  GAELIC("ga"),
+  WELSH("cy"),
+  ULSTERSCOTCH("eo"),
+  ENGLISH("en");
+  
+  private String isoLikeCode;
+
+  private Language(String isoLikeCode) {
+    this.isoLikeCode = isoLikeCode;
+  }
+
+  public String getIsoLikeCode() {
+    return isoLikeCode;
+  }
+  
+  
+}
